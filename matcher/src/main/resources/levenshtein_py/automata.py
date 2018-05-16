67c9f7b1b9528be63edce0179bb6964ea06ec89b
Alexander Myltsev <alexander@myltsev.com>
2018-05-16 16:16:00 +0300

    Encapsulate input word transform and matching to separate classes

--- matcher/src/main/resources/levenshtein_py/automata.py
+++ matcher/src/main/resources/levenshtein_py/automata.py
@@ -219,49 +219,49 @@ class Matcher(object):
             return None
 
 
-class Finder:
-    def __init__(self, words_datasources):
-        self.stem_to_words = {}
-        self.words_to_datasources = words_datasources
-        words_stems = []
-        for idx, (word, data_sources) in enumerate(words_datasources.iteritems()):
-            if idx % 100000 == 0:
+def _stemmize_word(word):
+    word_parts = word.split(' ')
+    word_stemmized = ' '.join(LatinStemmer.stemmize(word_part).stem for word_part in word_parts)
+    return word_stemmized
+
+
+def _find_all_matches(lev, lookup_func, lookup_ds):
+    match = lev.next_valid_string(u'\0')
+    while match:
+        nxt = lookup_func(match)
+        if not nxt:
+            return
+        if match == nxt:
+            if lookup_ds(match):
+                yield match
+            nxt = nxt + u'\0'
+        match = lev.next_valid_string(nxt)
+
+
+class MatcherByStem:
+    def __init__(self, words_to_datasources):
+        print("Constructing MatcherByStem")
+
+        self.word_stemmized_to_words = {}
+        self.words_to_datasources = words_to_datasources
+
+        for idx, (word, data_sources) in enumerate(words_to_datasources.iteritems()):
+            if idx > 0 and idx % 100000 == 0:
+                print word, data_sources
                 print(idx)
 
-            word_stemmized = self.stemmize_word(word)
+            word_stemmized = self.transform(word)
+            if word_stemmized not in self.word_stemmized_to_words:
+                self.word_stemmized_to_words[word_stemmized] = set()
+            self.word_stemmized_to_words[word_stemmized].add(word)
 
-            stem_words_set = self.stem_to_words.get(word_stemmized, set())
-            stem_words_set.add(word)
-            self.stem_to_words[word_stemmized] = stem_words_set
+        word_stemmized = list(self.word_stemmized_to_words.keys())
+        word_stemmized.sort()
+        self.bisect_by_word_stems = Matcher(word_stemmized)
+        pass
 
-            words_stems.append(word_stemmized)
-
-        self.words = list(words_datasources.keys())
-        self.words.sort()
-        self.words = Matcher(self.words)
-
-        words_stems.sort()
-        self.words_stems = Matcher(words_stems)
-
-    def stemmize_word(self, word):
-        word_parts = word.split(' ')
-        word_stemmized = ' '.join(LatinStemmer.stemmize(word_part).stem for word_part in word_parts)
-        return word_stemmized
-
-    def __find_all_matches(self, lev, lookup_func, lookup_ds):
-        match = lev.next_valid_string(u'\0')
-        while match:
-            nxt = lookup_func(match)
-            if not nxt:
-                return
-            if match == nxt:
-                if lookup_ds(match):
-                    yield match
-                nxt = nxt + u'\0'
-            match = lev.next_valid_string(nxt)
-
-    def __match_by_stem(self, word, data_sources):
-        word_stem = self.stemmize_word(word)
+    def match(self, word, data_sources):
+        word_stem = self.transform(word)
         lev = levenshtein_automata(word_stem).to_dfa()
 
         def lookup_ds(stem):
@@ -269,60 +269,166 @@ class Finder:
                 return True
             s = set(
                 ds
-                for w in self.stem_to_words[stem]
+                for w in self.word_stemmized_to_words[stem]
                 for ds in self.words_to_datasources[w]
             )
             intersect = s.intersection(data_sources)
             return len(intersect) > 0
 
-        res = list(self.__find_all_matches(lev, self.words_stems, lookup_ds))
+        res = list(_find_all_matches(lev, self.bisect_by_word_stems, lookup_ds))
         return res
 
-    def __match_by_full(self, word, data_sources):
-        lev = levenshtein_automata(word).to_dfa()
+    @staticmethod
+    def transform(word):
+        word_stemmized = _stemmize_word(word.lower())
+        return word_stemmized
 
-        def lookup_ds(w):
+    def lookup(self, word_transformed):
+        return self.word_stemmized_to_words.get(word_transformed, set())
+
+
+class MatcherByVerbatim:
+    def __init__(self, words_to_datasources):
+        print("Constructing MatcherByVerbatim")
+
+        self.words_to_datasources = words_to_datasources
+        self.words_verbatims_to_words = {}
+
+        for idx, word in enumerate(words_to_datasources.keys()):
+            if idx > 0 and idx % 100000 == 0:
+                print(idx)
+
+            word_verbatim = self.transform(word)
+
+            if word_verbatim not in self.words_verbatims_to_words:
+                self.words_verbatims_to_words[word_verbatim] = set()
+            self.words_verbatims_to_words[word_verbatim].add(word)
+
+        word_verbatims = list(self.words_verbatims_to_words.keys())
+        word_verbatims.sort()
+        self.bisect_by_word_verbatims = Matcher(word_verbatims)
+        pass
+
+    def match(self, word, data_sources):
+        word_verbatim = self.transform(word)
+        lev = levenshtein_automata(word_verbatim).to_dfa()
+
+        def lookup_ds(w_verb):
             if len(data_sources) == 0:
                 return True
-            s = set(ds for ds in self.words_to_datasources[w])
+            s = set(ds
+                    for w_orig in self.words_verbatims_to_words[w_verb]
+                    for ds in self.words_to_datasources[w_orig])
             intersect = s.intersection(data_sources)
             return len(intersect) > 0
 
-        res = list(self.__find_all_matches(lev, self.words, lookup_ds))
+        res = list(_find_all_matches(lev, self.bisect_by_word_verbatims, lookup_ds))
         return res
 
-    def find_all_matches(self, word, data_sources=set()):
-        word = re.sub('\s+', ' ', word.strip()).lower()
-        print(word)
+    @staticmethod
+    def transform(word):
+        word_verbatim = word.lower().replace('j', 'i').replace('v', 'u')
+        return word_verbatim
 
-        if ' ' not in word:
-            print('single word match')
-            if word in self.words_to_datasources:
-                res = [word]
-            else:
-                res = []
+    def lookup(self, word_transformed):
+        return self.words_verbatims_to_words.get(word_transformed, set())
+
+
+class MatcherByGenusOnly:
+    def __init__(self, words_to_datasources):
+        print("Constructing MatcherByGenusOnly")
+
+        self.words_to_datasources = words_to_datasources
+        self.words_genus_only_to_words = {}
+
+        for idx, word in enumerate(words_to_datasources.keys()):
+            if idx > 0 and idx % 100000 == 0:
+                print(idx)
+
+            word_transformed = self.transform(word)
+            if ' ' in word_transformed:
+                continue
+
+            if word_transformed not in self.words_genus_only_to_words:
+                self.words_genus_only_to_words[word_transformed] = set()
+            self.words_genus_only_to_words[word_transformed].add(word)
+
+        pass
+
+    def match(self, word, data_sources):
+        word_transformed = self.transform(word)
+        res = self.words_genus_only_to_words.get(word_transformed, set())
+        return [r.lower() for r in res]
+
+    @staticmethod
+    def transform(word):
+        word_verbatim = word.lower().replace('j', 'i').replace('v', 'u')
+        return word_verbatim
+
+    def lookup(self, word_transformed):
+        res = self.words_genus_only_to_words.get(word_transformed, set())
+        return res
+
+
+class Finder:
+    def __init__(self, words_to_datasources):
+        self.words_to_datasources = words_to_datasources
+        self.matcher_by_stem = MatcherByStem(words_to_datasources)
+        self.matcher_by_verbatim = MatcherByVerbatim(words_to_datasources)
+        self.matcher_by_genus_only = MatcherByGenusOnly(words_to_datasources)
+
+    def __pipeline(self, word, data_sources=set()):
+        word_cleaned = re.sub('\s+', ' ', word.strip()).lower()
+        print 'request: ', word_cleaned, '|', data_sources
+
+        matches_genus_only = self.matcher_by_genus_only.match(word_cleaned, data_sources)
+        if matches_genus_only:
+            print 'single word match', matches_genus_only
+            res = [
+                w
+                for match_genus_only in matches_genus_only
+                for w in self.matcher_by_genus_only.lookup(match_genus_only)
+            ]
+            print 'single word match (filtered)', res
+            return res
 
         else:
-            matches_by_stem = self.__match_by_stem(word, data_sources)
-            if len(matches_by_stem) > 0:
+            matches_by_stem = self.matcher_by_stem.match(word_cleaned, data_sources)
+            print 'matches_by_stem', matches_by_stem
+            if matches_by_stem:
                 res = [
                     w
                     for match_by_stem in matches_by_stem
-                    for w in self.stem_to_words[match_by_stem]
+                    for w in self.matcher_by_stem.lookup(match_by_stem)
                 ]
 
-                if len(data_sources) > 0:
+                if data_sources:
                     res = [r for r in res
                            if len(data_sources.intersection(self.words_to_datasources[r]))]
+                    print 'matches_by_stem (filtered)', res
 
             else:
-                res = self.__match_by_full(word, data_sources)
+                matches_by_verbatim = self.matcher_by_verbatim.match(word_cleaned, data_sources)
+                res = [
+                    w
+                    for match_by_verbatim in matches_by_verbatim
+                    for w in self.matcher_by_verbatim.lookup(match_by_verbatim)
+                ]
+                print 'matches_by_verbatim', matches_by_verbatim
 
-                if len(data_sources) > 0:
+                if data_sources:
                     res = [r for r in res
                            if len(data_sources.intersection(self.words_to_datasources[r]))]
-
-        res = [r[0].upper() + r[1:] for r in res]
+                    print 'matches_by_verbatim (filtered)', res
 
         print 'res:', res
         return res
+
+    def find_all_matches(self, word, data_sources=set()):
+        import traceback
+        try:
+            return self.__pipeline(word, data_sources)
+        except Exception as ex:
+            print ex
+            traceback.print_exc()
+            return []
